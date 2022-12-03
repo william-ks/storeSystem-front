b552409602b19c6740434f1c775697cb21c052e5
otavio <otavioagusutomperes2703@gmail.com>
2022-12-02 22:04:12 -0300

    style: estilização inicial da pagina de login

--- src/pages/login/style.js
+++ src/pages/login/style.js
@@ -1,7 +1,37 @@
 import styled from "styled-components";
 
 const Container = styled.div`
+    min-height: 100vh;
 
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
+    text-align: center;
+
+    p{
+        text-align: left;
+    }
+
+    a{
+        text-align: right;
+        display: block;
+    }
+
+    .loginForm {
+        width: clamp(130px, 90%, 500px);
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+
+    }
+
+    button{
+        width: clamp(100px, 80%, 300px);
+        margin: 0 auto;
+    }
 `;
 
 export default Container;
